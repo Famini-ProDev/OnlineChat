77eaf1392bacc48d0a7d7ac3835df7404331200a
Famini-ProDev <faminiprodev@gmail.com>
2022-04-18 19:49:53 +0430

    added files onlineChat

---
+++ front_app/src/pages/login/style.js
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(theme => ({
+  headerText: {
+    width: 'max-content',
+    margin: '2rem auto 3rem auto',
+    fontSize: '3rem',
+    color: 'brown',
+  },
+  label: {
+    fontSize: '1rem'
+  },
+  formControl1: {
+    width: '10rem'
+  },
+  button: {
+    fontSize: '1rem',
+    margin: '2rem auto 0 auto',
+  },
+
+}))
